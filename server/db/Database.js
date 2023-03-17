1822521b8d3766408597a71bc34bea4545baaf04
shweta121212 <128180383+shweta121212@users.noreply.github.com>
2023-03-18 00:19:48 +0530

    Update Database.js

--- server/db/Database.js
+++ server/db/Database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/Form", {
+    .connect("mongodb+srv://shwetarathore0105:shweta1234@cluster0.klnwtob.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
